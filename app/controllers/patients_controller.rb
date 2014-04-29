1ed39d5dfcf13596d3a9c0d75ec9b88c00a56b10
Jatin Bhandari <jatin.bhandari@mail.utoronto.ca>
2014-04-29 17:18:13 -0400

    fixes indentation

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -17,14 +17,14 @@ class PatientsController < ApplicationController
 
   def patient_params
     params.require(:patient).permit(
-    :first_name,
-    :last_name,
-    :gender,
-    :age,
-    :address,
-    :phone_number,
-    :insurance_provider,
-    :policy_number)
+      :first_name,
+      :last_name,
+      :gender,
+      :age,
+      :address,
+      :phone_number,
+      :insurance_provider,
+      :policy_number)
   end
 
 end
