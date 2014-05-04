1a9a7c8185c25e4ee71dc29cb89af18df2d4500c
Jatin Bhandari <jatin.bhandari@mail.utoronto.ca>
2014-05-03 23:38:09 -0400

    clear whitespace

--- app/models/script.rb
+++ app/models/script.rb
@@ -2,4 +2,5 @@ class Script < ActiveRecord::Base
 
   has_one :physician
   has_one :patient
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
+  root 'homes#index'
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
-  root 'homes#index'
   resources :homes, only: [:show]
   resources :clinics, only: [:edit, :update, :index]
   resources :physicians, only: [:edit, :update, :show, :index]
   resources :patients, only: [:edit, :update, :index]
   resources :scripts, only: [:new, :show]
   resource :search, only: [:show, :create]
+
 end
