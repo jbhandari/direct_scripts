80fbc2aef485d836aee51759c68035c8e0e29495
Jatin Bhandari <jatin.bhandari@mail.utoronto.ca>
2014-05-06 15:07:38 -0400

    adds patient search

---
+++ app/controllers/patient_finders_controller.rb
@@ -0,0 +1,10 @@
+class PatientFindersController < ApplicationController
+
+  def new
+  end
+
+  def show
+    @patient = Patient.find(params[:patient_id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
 
   root 'homes#index'
+
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   resources :homes, only: [:show]
   resources :clinics, only: [:edit, :update, :index]
   resources :physicians, only: [:edit, :update, :show, :index]
-  resources :patients, only: [:edit, :update, :index]
-  resources :scripts, only: [:new, :show]
+  resource :patient_finders, only: [:new]
   resource :search, only: [:show, :create]
+
+  resources :patients, only: [:edit, :update, :index] do
+    resources :scripts, only: [:new, :create]
+  end
 
 end
